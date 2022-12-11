79a7c65244ad89cb5c09d2b0e475fd2496e69747
michaldoman <mjd.domanski@gmail.com>
2022-12-11 16:18:41 +0100

    calculates both scores and updates them

--- 2001.py
+++ 2001.py
@@ -51,6 +51,7 @@ def main():
     if request.method == 'GET':
         return html_init
     elif request.method == 'POST':
+        # Variables:
         dice_types = ['D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100']
         dice_types_str = ', '.join(dice_types)
         turn = int(request.form['turn'])
@@ -58,6 +59,7 @@ def main():
         computer_score = int(request.form['computer_score'])
         player_score = int(request.form['player_score'])
 
+        # Calculates user's score:
         throw_1 = request.form['throw_1']
         throw_2 = request.form['throw_2']
         if throw_1.upper() not in dice_types or throw_2.upper() not in dice_types:
@@ -67,9 +69,21 @@ def main():
         dice_2_size = int(throw_2.upper().strip('D'))
         throw_1 = randint(1, dice_1_size)
         throw_2 = randint(1, dice_2_size)
+
+        # Calculates computer's score:
+        comp_dice_1_code = dice_types[randint(0, len(dice_types) - 1)]
+        comp_dice_2_code = dice_types[randint(0, len(dice_types) - 1)]
+        comp_dice_1_size = int(comp_dice_1_code.upper().strip('D'))
+        comp_dice_2_size = int(comp_dice_2_code.upper().strip('D'))
+        comp_throw_1 = randint(1, comp_dice_1_size)
+        comp_throw_2 = randint(1, comp_dice_2_size)
+
         if turn > 0:
             player_score += (throw_1 + throw_2)
-            throw_announcement = f'''\n Your throws:  {throw_1} ,  {throw_2}'''
+            computer_score += (comp_throw_1 + comp_throw_2)
+            throw_announcement = f'''\n Your throws:  {throw_1} ,  {throw_2}\n
+            Computer dice types:  {comp_dice_1_code} ,  {comp_dice_2_code}\n
+            Computer throws:  {comp_throw_1} ,  {comp_throw_2}'''
 
 
         turn += 1
